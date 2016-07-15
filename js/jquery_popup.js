dacce5435dca33da4adc124903519d741dd1fc40
mrcarlossalazar <mr.carlos.salazar@gmail.com>
2016-07-15 12:18:50 -0400

    a buncha shit

---
+++ js/jquery_popup.js
@@ -0,0 +1,32 @@
+$(document).ready(function()
+    {
+        $("#onclick").click(function() {
+            $(window).scrollTo(0,0);
+            $("#contactdiv").fadeIn('slow')
+            $("#heading").fadeOut('slow');
+            $("#introtext").fadeOut('slow');
+            $("#brideandgroom").fadeOut('slow');
+            $("#accommodations").fadeOut('slow');
+            $("#wedding").fadeOut('slow');
+            $("#registry").fadeOut('slow');     
+
+        });
+        $("#cancel").click(function() {
+            $(this).parent().fadeOut('slow');
+            $("#heading").fadeIn('slow');
+            $("#introtext").fadeIn('slow');
+            $("#brideandgroom").fadeIn('slow');
+            $("#accommodations").fadeIn('slow');
+            $("#wedding").fadeIn('slow');
+            $("#registry").fadeIn('slow');
+        });
+        $("#submit").click(function() {
+            $(this).parent().fadeOut('slow');
+            $("#heading").fadeIn('slow');
+            $("#introtext").fadeIn('slow');
+            $("#brideandgroom").fadeIn('slow');
+            $("#accommodations").fadeIn('slow');
+            $("#wedding").fadeIn('slow');
+            $("#registry").fadeIn('slow');
+        });
+    });
