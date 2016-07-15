dd904c3372528baa01a2c95e074d02cc6b25f1b0
mrcarlossalazar <mr.carlos.salazar@gmail.com>
2016-07-15 14:14:09 -0400

    added vars config

--- send_heroku.php
+++ send_heroku.php
@@ -29,7 +29,9 @@ if ($conn->connect_error) {
 
 // sql to create table
 
-$sql = "INSERT INTO guests (firstname,lastname, email,state,dinner,address,address2,zipcode,phonenumber,date) VALUES ('$firstname', '$lastname','$email','$states','$dinner','$address','$address2','$zipcode','$phonenumber','$date')";
+$sql = "SELECT DATABASE()";
+
+"INSERT INTO guests (firstname,lastname, email,state,dinner,address,address2,zipcode,phonenumber,date) VALUES ('$firstname', '$lastname','$email','$states','$dinner','$address','$address2','$zipcode','$phonenumber','$date')";
 
 if ($conn->query($sql) === TRUE) {
     echo "Guests added to RVSP List successfully  ";
