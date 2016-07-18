e969dbbf45bff944cfda5eb89a878baada4c1b11
mrcarlossalazar <mr.carlos.salazar@gmail.com>
2016-07-18 14:44:09 -0400

    test

--- send_heroku.php
+++ send_heroku.php
@@ -26,9 +26,7 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 } 
 
-$sql = "USE DATABASE enu1agq34j8fjzya";
-
-"INSERT INTO guests (firstname,lastname,email,state,dinner,address,address2,zipcode,phonenumber,date) VALUES ('$firstname', '$lastname','$email','$states','$dinner','$address','$address2','$zipcode','$phonenumber','$date')";
+$sql = "SELECT DATABASE enu1agq34j8fjzya";
 
 if ($conn->query($sql) === TRUE) {
     echo "Guests added to RVSP List successfully  ";
