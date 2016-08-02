5bcf16c6e0aad367944b4e7259a7268df0368b96
mrcarlossalazar <mr.carlos.salazar@gmail.com>
2016-08-02 10:08:08 -0400

    changed  to

--- submitmusic.php
+++ submitmusic.php
@@ -6,21 +6,17 @@ $hostname = $dbparts['host'];
 $username = $dbparts['user'];
 $password = $dbparts['pass'];
 $database = ltrim($dbparts['path'],'/');
-
-
 $artist=$_POST['artist'];
 $song=$_POST['song'];
 $yt=$_POST['yt'];
 
 // Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
+$conn = new mysqli($servername, $username, $password, $database);
 // Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 } 
-
 // sql to create table
-
 $sql = "INSERT INTO music (artist,song, yt) VALUES ('$artist','$song','$yt')";
 
 if ($conn->query($sql) === TRUE) {
@@ -29,7 +25,6 @@ if ($conn->query($sql) === TRUE) {
 } else {
     echo "Try Again " . $conn->error;
 }
-
 //header("Location:/wedding/home.html?");
 $conn->close();
 ?>
