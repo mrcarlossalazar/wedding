a67b0aca3f8709034a60bb35dac1480cfb5c7864
mrcarlossalazar <mr.carlos.salazar@gmail.com>
2016-08-02 17:23:31 -0400

    added show music functionality

---
+++ showmusic.php
@@ -0,0 +1,37 @@
+<?php
+
+$url = getenv('JAWSDB_URL');
+$dbparts = parse_url($url);
+
+$hostname = $dbparts['host'];
+$username = $dbparts['user'];
+$password = $dbparts['pass'];
+$database = ltrim($dbparts['path'],'/');
+
+      $con= new mysqli($hostname, $username, $password, $database);
+      $table = "<table border=1>";
+
+      $name = $_POST['search'];
+
+      if (mysqli_connect_errno())
+      {
+      echo "Failed to connect to the database: " . mysqli_connect_error();
+      }
+      $result = mysqli_query($con, "SELECT *  FROM music");
+
+      while ($row = mysqli_fetch_array($result))
+      {
+ $table .= "<tr><td>" . $row['music_id'] . " </td><td>" . $row['artist'] . " </td><td>" . $row['song'] . " </td><td>" .  $row['yt'] . "<br></td></tr>";
+    $table .= "</table>";
+
+      }
+      echo $table;
+      sleep(1);
+
+     // header("Location:/wedding/home.html?");
+      mysqli_close($con);
+?> 
+
+
+
+
