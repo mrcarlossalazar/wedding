5e972f86304fc1d3f00e6643912d297361e75789
mrcarlossalazar <mr.carlos.salazar@gmail.com>
2016-08-02 10:14:05 -0400

    redirect

--- submitmusic.php
+++ submitmusic.php
@@ -22,9 +22,12 @@ $sql = "INSERT INTO music (artist,song, yt) VALUES ('$artist','$song','$yt')";
 if ($conn->query($sql) === TRUE) {
     echo "Your request has been submitted";
     sleep(1);
+    header("Location:/wedding/home.html?");
+
 } else {
     echo "Try Again " . $conn->error;
+    header("Location:/wedding/home.html?");
+
 }
-header("Location:/wedding/home.html?");
 $conn->close();
 ?>
