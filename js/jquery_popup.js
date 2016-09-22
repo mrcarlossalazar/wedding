184ae1988a48b4fb757a64ea22f5ab5dfe277e68
mrcarlossalazar <mr.carlos.salazar@gmail.com>
2016-09-22 14:15:59 -0400

    added remove music and add music to functions

--- js/jquery_popup.js
+++ js/jquery_popup.js
@@ -2,13 +2,15 @@ $(document).ready(function()
     {
         $("#popup").click(function() {
             $(window).scrollTo(0,0);
-            $("#contactdiv").fadeIn('slow')
+            $("#contactdiv").fadeIn('fast')
             $("#heading").fadeOut('slow');
             $("#introtext").fadeOut('slow');
             $("#brideandgroom").fadeOut('slow');
             $("#accommodations").fadeOut('slow');
             $("#wedding").fadeOut('slow');
-            $("#registry").fadeOut('slow');     
+            $("#registry").fadeOut('slow');
+            $("#music").fadeOut('slow');     
+     
 
         });
         $("#cancel").click(function() {
@@ -19,14 +21,19 @@ $(document).ready(function()
             $("#accommodations").fadeIn('slow');
             $("#wedding").fadeIn('slow');
             $("#registry").fadeIn('slow');
+            $("#music").fadeIn('slow');     
+
         });
         $("#submit").click(function() {
             $(this).parent().fadeOut('slow');
+            $("#contactdiv").fadeOut('slow')
             $("#heading").fadeIn('slow');
             $("#introtext").fadeIn('slow');
             $("#brideandgroom").fadeIn('slow');
             $("#accommodations").fadeIn('slow');
             $("#wedding").fadeIn('slow');
             $("#registry").fadeIn('slow');
+            $("#music").fadeIn('slow');     
+
         });
     });
