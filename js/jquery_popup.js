2c6b68dd23c57ed16b789c6b799718e4992d776e
mrcarlossalazar <mr.carlos.salazar@gmail.com>
2016-09-22 12:07:26 -0400

    test

--- js/jquery_popup.js
+++ js/jquery_popup.js
@@ -11,22 +11,5 @@ $(document).ready(function()
             $("#registry").fadeOut('slow');     
 
         });
-        $("#cancel").click(function() {
-            $(this).parent().fadeOut('slow');
-            $("#heading").fadeIn('slow');
-            $("#introtext").fadeIn('slow');
-            $("#brideandgroom").fadeIn('slow');
-            $("#accommodations").fadeIn('slow');
-            $("#wedding").fadeIn('slow');
-            $("#registry").fadeIn('slow');
-        });
-        $("#submit1").click(function() {
-            $(this).parent().fadeOut('slow');
-            $("#heading").fadeIn('slow');
-            $("#introtext").fadeIn('slow');
-            $("#brideandgroom").fadeIn('slow');
-            $("#accommodations").fadeIn('slow');
-            $("#wedding").fadeIn('slow');
-            $("#registry").fadeIn('slow');
-        });
+
     });
