c1c1c3b7b30c03c8ded1eb8d77d2fb0d51564601
mrcarlossalazar <mr.carlos.salazar@gmail.com>
2016-09-22 12:17:57 -0400

    removed fadeouts

--- js/jquery_popup.js
+++ js/jquery_popup.js
@@ -3,12 +3,7 @@ $(document).ready(function()
         $("#onclick").click(function() {
             $(window).scrollTo(0,0);
             $("#contactdiv").fadeIn('slow')
-            $("#heading").fadeOut('slow');
-            $("#introtext").fadeOut('slow');
-            $("#brideandgroom").fadeOut('slow');
-            $("#accommodations").fadeOut('slow');
-            $("#wedding").fadeOut('slow');
-            $("#registry").fadeOut('slow');     
+   
 
         });
         $("#cancel").click(function() {
@@ -22,11 +17,6 @@ $(document).ready(function()
         });
         $("#submit").click(function() {
             $(this).parent().fadeOut('slow');
-            $("#heading").fadeIn('slow');
-            $("#introtext").fadeIn('slow');
-            $("#brideandgroom").fadeIn('slow');
-            $("#accommodations").fadeIn('slow');
-            $("#wedding").fadeIn('slow');
-            $("#registry").fadeIn('slow');
+
         });
     });
