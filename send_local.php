7bb053a9378ac2866a28696905fee31699ca14c1
mrcarlossalazar <mr.carlos.salazar@gmail.com>
2016-09-22 11:40:06 -0400

    changed button and ID

--- send_local.php
+++ send_local.php
@@ -1,16 +1,17 @@
 <?php
-$servername = "localhost";
-$username = "OBDC";
-$password = "";
-$dbname = "test";
-$date = new DateTime();
+$url = getenv('JAWSDB_URL');
+$dbparts = parse_url($url);
 
+$hostname = $dbparts['host'];
+$username = $dbparts['user'];
+$password = $dbparts['pass'];
+$database = ltrim($dbparts['path'],'/');
 
 $firstname=$_POST['firstname'];
 $lastname=$_POST['lastname'];
 $email=$_POST['email'];
 $states=$_POST['states'];
-$dinner=$_POST['dinner_selection'];
+$dinner=$_POST['dinner'];
 $address=$_POST['address'];
 $address2=$_POST['address2'];
 $zipcode=$_POST['zipcode'];
@@ -18,21 +19,22 @@ $phonenumber=$_POST['phonenumber'];
 $date=$_POST['date'];
 
 // Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
+$conn = new mysqli($hostname, $username, $password, $database);
 // Check connection
 if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-} 
+    die("Connection failed: " . $conn->connect_error);}
 
-// sql to create table
-
-
+$sql = "INSERT INTO guests (firstname,lastname, email,states,dinner,address,address2,zipcode,phonenumber,date) VALUES ('$firstname', '$lastname','$email','$states','$dinner','$address','$address2','$zipcode','$phonenumber','$date')";
 
 if ($conn->query($sql) === TRUE) {
-    echo "Guests added to RVSP List successfully ";
+    echo "Guest(s) registered successfully";
 } else {
-    echo "Error RSVPing: " . $conn->error;
+    echo "Error, could not register: " . $conn->error;
 }
 
 $conn->close();
+	sleep(1);
+
+      header("Location:/wedding/home.html");
+
 ?>
