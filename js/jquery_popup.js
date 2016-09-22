8061ec3085c8bebd01e70e170ae4ea3b67f1753b
mrcarlossalazar <mr.carlos.salazar@gmail.com>
2016-09-22 14:06:03 -0400

    added sections back

--- js/jquery_popup.js
+++ js/jquery_popup.js
@@ -1,9 +1,14 @@
 $(document).ready(function()
     {
-        $("#onclick").click(function() {
+        $("#popup").click(function() {
             $(window).scrollTo(0,0);
             $("#contactdiv").fadeIn('slow')
-   
+            $("#heading").fadeOut('slow');
+            $("#introtext").fadeOut('slow');
+            $("#brideandgroom").fadeOut('slow');
+            $("#accommodations").fadeOut('slow');
+            $("#wedding").fadeOut('slow');
+            $("#registry").fadeOut('slow');     
 
         });
         $("#cancel").click(function() {
@@ -17,6 +22,11 @@ $(document).ready(function()
         });
         $("#submit").click(function() {
             $(this).parent().fadeOut('slow');
-
+            $("#heading").fadeIn('slow');
+            $("#introtext").fadeIn('slow');
+            $("#brideandgroom").fadeIn('slow');
+            $("#accommodations").fadeIn('slow');
+            $("#wedding").fadeIn('slow');
+            $("#registry").fadeIn('slow');
         });
     });
