1ca879a6fd007c1e8e34776cf6ddd82daea5d1c1
mrcarlossalazar <mr.carlos.salazar@gmail.com>
2016-09-23 10:15:00 -0400

    rollback

--- showmusic.php
+++ showmusic.php
@@ -9,8 +9,7 @@ $password = $dbparts['pass'];
 $database = ltrim($dbparts['path'],'/');
 
       $con= new mysqli($hostname, $username, $password, $database);
-      $table = "<link type="text/css" media="screen" rel="stylesheet" href="css/responsive-tables.css" />
-<table class="responsive">" ."<td>Artist</td>"."<td>Song Title</td>"."<td>Youtube Link</td>";
+      $table = "<table border=1>" ."<td>Artist</td>"."<td>Song Title</td>"."<td>Youtube Link</td>";
 
       if (mysqli_connect_errno()){
       echo "Failed to connect to the database: " . mysqli_connect_error();
@@ -25,8 +24,7 @@ $database = ltrim($dbparts['path'],'/');
                    $table .= "<td><a href=".$row[yt].">".$row[song]."</td>";
                    $table .= "</tr>";
       }
-      $table .= "</table><script type="text/javascript" src="js/responsive-tables.js"></script>
-";
+      $table .= "</table>";
       echo $table;
       echo '<form><button formaction="https://salazarwedding.herokuapp.com/">Go Back</button></form>';
       mysqli_close($con);
