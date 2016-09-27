27cc6a4677a0df482a0fecbf088e3f21596f0b28
mrcarlossalazar <mr.carlos.salazar@gmail.com>
2016-09-27 11:54:21 -0400

    removed extra <

--- showmusic.php
+++ showmusic.php
@@ -19,7 +19,7 @@ $password = $dbparts['pass'];
 $database = ltrim($dbparts['path'],'/');
 
       $con= new mysqli($hostname, $username, $password, $database);
-      $table ='<table border="1|0" class = "responsive">' . ">" ."<td>Artist</td>"."<td>Song Title</td>"."<td>Youtube Link</td>";
+      $table ='<table border="1|0" class = "responsive">'."<td>Artist</td>"."<td>Song Title</td>"."<td>Youtube Link</td>";
 
       if (mysqli_connect_errno()){
       echo "Failed to connect to the database: " . mysqli_connect_error();
