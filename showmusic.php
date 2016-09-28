cd50865126ff44708a381481e5436a8f3d1af26b
mrcarlossalazar <mr.carlos.salazar@gmail.com>
2016-09-28 15:23:39 -0400

    added header

--- showmusic.php
+++ showmusic.php
@@ -39,12 +39,14 @@
       </div>
       <div id="bg-container">
             
-            <div class="main-container">
+            <div class="main-container">                        
+            <h1>Music Requests</h1>
+
                   <div class="main wrapper clearfix">
                          <section>
                         <h1>Spotify Requests</h1>
                               <div class="column full">
-                        <iframe src="https://embed.spotify.com/?uri=spotify%3Auser%3A12181151300%3Aplaylist%3A48eZqdIOuon1uS1CKyIp4M" width="100%" height="50%" frameborder="1" allowtransparency="true"></iframe>
+                              <iframe src="https://embed.spotify.com/?uri=spotify%3Auser%3A12181151300%3Aplaylist%3A48eZqdIOuon1uS1CKyIp4M" width="100%" frameborder="1" allowtransparency="true"></iframe>
                               <form><button class="button round" formaction="https://salazarwedding.herokuapp.com/">Go Back</button></form>
                               </div><!--end column-->
                         </section>
