ed7fb6de3178dd49fc490a34184240ef29f46a3d
mrcarlossalazar <mr.carlos.salazar@gmail.com>
2016-09-28 14:47:25 -0400

    missing /

--- showmusic.php
+++ showmusic.php
@@ -41,7 +41,7 @@
             
             <div class="main-container">
                   <div class="main wrapper clearfix">
-                        <h1> Current Spotify List<h1>
+                        <h1> Current Spotify List</h1>
                         <iframe src="https://embed.spotify.com/?uri=spotify%3Auser%3A12181151300%3Aplaylist%3A48eZqdIOuon1uS1CKyIp4M" width="100%" height="50%" frameborder="1" allowtransparency="true"></iframe>
                         <section>
                               <h1>Music Requests</h1>
