ebb324d1576f71c7a57801e94f44023265e17214
mrcarlossalazar <mr.carlos.salazar@gmail.com>
2016-09-28 14:33:46 -0400

    what is this spoitify?

--- showmusic.php
+++ showmusic.php
@@ -10,6 +10,9 @@
             <style type="text/css"> section {height: 250%;}</style>
       </head>
       <body>
+            <img src="img/spotify.png" c alt="Click to Add Your Music Requests!" />
+            <h2> Current Spotify List<h2>
+            <iframe src="https://embed.spotify.com/?uri=spotify%3Auser%3A12181151300%3Aplaylist%3A48eZqdIOuon1uS1CKyIp4M" width="100%" height="100%" frameborder="1" allowtransparency="true"></iframe>
             <?php
             $url = getenv('JAWSDB_URL');
             $dbparts = parse_url($url);
